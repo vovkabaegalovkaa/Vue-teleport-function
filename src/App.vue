f23d2a281fcddb9bee16967f683b5cebe88492aa
Vova <vovagotovko2004@gmail.com>
2024-08-20 15:41:10 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <h2>Teleports</h2>
+    <button class="btn" @click="modalStatus = true">Открыть модальное окно</button>
+    <teleport to="body">
+      <app-modal v-if="modalStatus" @closeModal="modalStatus = false"></app-modal>
+    </teleport>
+    <!-- Теперь наше модальное окно врезается в DOM и хоть оно и выглядит нормально, все равно структура сайта рушится, с этим и 
+    помогут телепорты
+    Телепорт – это тег, которого нет в HTML, а добавляет его сюда именно Vue. 
+    У тега есть два аттрибута: to – отвечает за то, в какую область DOM будет перенесено содержимое тега(указывается через css-селектор)
+    Аттирбут disabled для котрого не нужны значения, он просто отменяет телепортацию, ну мало ли что-->
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppModal from './components/AppModal.vue';
 export default {
-  name: 'App',
+  data(){
+    return {
+      modalStatus: false,
+    }
+  },
   components: {
-    HelloWorld
+    'app-modal': AppModal,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/AppModal.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="modal-back" @click="$emit('closeModal')"></div>
+    <div class="modal">
+        <h3>Заголовок модального окна</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Exercitationem, commodi! Quia aliquid asperiores nobis ex eius repellendus eligendi quasi et?</p>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ['closeModal']
+}
+</script>
